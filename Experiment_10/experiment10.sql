aee1c49535d56e1ae791c0ddcaaa63ff29a0409f
Mimanshu Gahlaut <mimanshugahlaut10@gmail.com>
2026-04-24 19:04:25 +0530

    Create employees table with auto-incrementing ID

---
+++ Experiment_10/experiment10.sql
@@ -0,0 +1,31 @@
+CREATE TABLE employees1 (
+    emp_id INTEGER PRIMARY KEY,
+    emp_name VARCHAR(100),
+    department VARCHAR(50)
+);
+
+CREATE SEQUENCE emp_seq
+START 1;
+
+CREATE OR REPLACE FUNCTION auto_emp_id()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.emp_id IS NULL THEN
+        NEW.emp_id := nextval('emp_seq');
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auto_emp_id
+BEFORE INSERT ON employees1
+FOR EACH ROW
+EXECUTE FUNCTION auto_emp_id();
+
+INSERT INTO employees1 (emp_name, department)
+VALUES ('AliceD', 'HR');
+
+INSERT INTO employees1 (emp_name, department)
+VALUES ('Bob', 'IT');
+
+SELECT * FROM employees1;
